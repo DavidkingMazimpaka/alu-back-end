baa8be689f6a72ed821bcb81e3fdf78380bacce8
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2023-02-23 18:16:58 +0200

    py: api export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python3
-""" 
-Exporting to CSV
-"""
 
+"""
+Export to CSV.
+"""
 
 import csv
 import json
 import requests
 import sys
-if __name__ == "__main__":
-    link = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
-    res = requests.get(link)
-    user = json.loads(res.text)
-    num = sys.argv[1]
-    link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(num)
-    res = requests.get(link)
-    todos = json.loads(res.text)
-    csv_data = [["{}".format(i["userId"]),
-                user["username"],
-                "{}".format(i["completed"]),
-                 i["title"]] for i in todos]
-    with open("{}.csv".format(user["id"]), 'w', encoding='utf-8') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+
+
+def main():
+    # Get user ID from command-line arguments.
+    user_id = sys.argv[1]
+
+    # Fetch user data from API.
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    user_response = requests.get(user_url)
+    user = json.loads(user_response.text)
+
+    # Fetch user's to-do list from API.
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    todo_response = requests.get(todo_url)
+    todos = json.loads(todo_response.text)
+
+    # Prepare CSV data.
+    csv_data = [
+        [
+            str(todo["userId"]),
+            user["username"],
+            str(todo["completed"]),
+            todo["title"]
+        ]
+        for todo in todos
+    ]
+
+    # Write CSV file.
+    filename = f"{user['id']}.csv"
+    with open(filename, "w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
         writer.writerows(csv_data)
-        
+
+
+if __name__ == "__main__":
+    main()
