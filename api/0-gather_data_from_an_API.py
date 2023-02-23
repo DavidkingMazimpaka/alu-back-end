2cdc837b51ae02acf4b415aa93dd68ab85a5693b
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2023-02-23 18:05:14 +0200

    py: api TODO list

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,14 +4,16 @@ This script retrieves a to-do list for a specific user from a RESTful API and
 prints out the tasks that the user has completed.
 """
 
-import json
-import requests
 import sys
+
+import requests
+
 
 # Define a usage function to print a usage message and exit
 def usage():
     print("Usage: {} <user_id>".format(sys.argv[0]))
     sys.exit(1)
+
 
 if __name__ == "__main__":
     # Check if a user ID was provided as a command-line argument
@@ -22,26 +24,26 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
-    
+
     try:
         user_res = requests.get(user_url)
-        user_res.raise_for_status() # Raise an error if the request failed
+        user_res.raise_for_status()  # Raise an error if the request failed
         user = user_res.json()
-        
+
         todo_res = requests.get(todo_url)
-        todo_res.raise_for_status() # Raise an error if the request failed
+        todo_res.raise_for_status()  # Raise an error if the request failed
         todos = todo_res.json()
-        
+
     except (requests.exceptions.RequestException, ValueError) as e:
         print("Error: {}".format(e))
         sys.exit(1)
-        
+
     # Filter the completed tasks
     done = [t for t in todos if t['completed']]
-    
+
     # Print the results
     print('Employee {} is done with tasks ({}/{}):'.format(
         user['name'], len(done), len(todos)))
-    
+
     for t in done:
         print("\t" + t["title"])
